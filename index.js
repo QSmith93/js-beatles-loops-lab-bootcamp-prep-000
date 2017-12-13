35d5bde1cfa72d630406aeead57f7d3295f5cec6
Quentin <q.smith1@yahoo.com>
2017-12-13 04:21:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ function johnLennonFacts(facts) {
    }
    return newFacts
  }
+ 
+ 
+function iLoveTheBeatles(number) {
+  var phrase = []
+  do {
+     phrase.push("I love the Beatles!");
+     number++
+  do {
+     phrase.push("I love the Beatles!");
+   } while (number < 15)
+   return phrase
+  }
